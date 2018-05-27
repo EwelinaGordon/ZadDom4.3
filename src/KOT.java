e36c0cd3652ae1956ba8a2a5d03001a45e8d16cf
EwelinaGordon <ewelina.gordon@gmail.com>
2018-05-27 17:10:54 +0200

    Zadanie 4.3

---
+++ src/KOT.java
@@ -0,0 +1,19 @@
+public class KOT {
+    public static void main(String[] args) {
+        int  a = 75;
+        char aa = (char) a;
+        int  b = 79;
+        char bb = (char) b;
+        int  c = 84;
+        char cc = (char) c;
+
+        System.out.print(aa);
+        System.out.print(bb);
+        System.out.print(cc);
+    }
+
+
+
+
+
+}
